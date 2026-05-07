b287a67607c3b34822c6c0754306106e405b5dcd
JeanLukas <lukasjean745@gmail.com>
2026-05-07 15:45:14 -0300

    routers and main.py updated

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.routers.chrome_routers import router
 
 app = FastAPI(debug= True, title="API para gerenciar chromebooks do colegio", servers=[{"url": "http://127.0.0.1:8000", "description": "Dev Environment"}])
+
+app.include_router(router)
